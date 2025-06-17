6e09455bbe0775dbca4bf2683ccf0e81fbc0ca50
Muneer Hamdan <muneerhamdan01@gmail.com>
2025-06-17 10:41:38 -0400

    done except sfRenderWindow_create() params

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ file=fut
 all: $(file)
 
 $(file):
-	gcc $(file).c -o $(file).out -Wall -Werror -fsanitize=undefined,address -g -lcsfml-graphics -lcsfml-window
+	gcc $(file).c -o $(file).out -Wall -Werror -fsanitize=undefined,address -g -lcsfml-graphics -lcsfml-window -lcsfml-system
 	./$(file).out
 
 clean:
--- fut.c
+++ fut.c
@@ -1,35 +1,39 @@
-#include </usr/include/CSFML/Graphics.h>
-#include </usr/include/CSFML/Window.h>
-#include <stdio.h>
- 
-#define W 600
-#define H 400
-#define GRAY (sfColor){18,18,18,18}
-#define GREEN (sfColor){33, 233, 33, 11}
-int main() {
+#include <CSFML/Graphics.h>
+#include <stdlib.h>
 
-	sfRenderWindow* window = sfRenderWindow_create((sfVideoMode){(sfVector2u){W, H}, (unsigned int)8}, (const char*)"window", (uint32_t)0, (sfWindowState)0, &(const sfContextSettings){24,8,16,4,6,1,1});
-	sfRenderWindow_setPosition(window, (sfVector2i){300, 300});
+#define GRAY (sfColor){18, 18, 18, 255}
+#define GREEN (sfColor){33, 211, 177, 255}
+
+int main(void)
+{
+	sfRenderWindow*   window = sfRenderWindow_create((const sfVideoMode){{800, 600}, 32}, "SFML window", sfResize | sfClose, sfWindowed, NULL);
+	if (!window)
+		return EXIT_FAILURE;
 
 
 	sfCircleShape* circle = sfCircleShape_create();
-	sfCircleShape_setPosition(circle, (sfVector2f){250.0, 250.0});
+	sfCircleShape_setPosition(circle, (sfVector2f){200, 200});
 	sfCircleShape_setFillColor(circle, GREEN);
-	sfCircleShape_setRadius(circle, 300.0);
+	sfCircleShape_setRadius(circle, 20.0);
 
 
-	int eventbool = 0;
-	while (sfRenderWindow_isOpen(window)) {
-
-		sfEvent e;
-		while ((eventbool = sfRenderWindow_pollEvent(window, &e)) == 1) {
-
-			if (e.type == sfEvtClosed) {
+	sfEvent event;
+	while (sfRenderWindow_isOpen(window))
+	{
+		while (sfRenderWindow_pollEvent(window, &event))
+		{
+			if (event.type == sfEvtClosed)
 				sfRenderWindow_close(window);
-			}
-			else if (e.type == sfEvtKeyPressed) {
-				if (e.key.code == sfKeyEscape) {
-					sfRenderWindow_close(window);
+			if (event.type == sfEvtKeyPressed) {
+				switch (event.key.code) {
+					case (sfKeyEscape):
+						sfRenderWindow_close(window);
+						break;
+					case (sfKeyH):
+						sfCircleShape_move(circle, (sfVector2f){-10.0, 0.0});
+						break;
+					default:
+						break;
 				}
 			}
 		}
@@ -37,9 +41,6 @@ int main() {
 		sfRenderWindow_clear(window, GRAY);
 
 		sfRenderWindow_drawCircleShape(window, circle, NULL);
-//		printf("%d ", sfRenderWindow_getSize(window).x);
-//		printf("%d ", sfRenderWindow_getPosition(window).x);
-		//printf("%f ", sfCircleShape_getRadius(circle));
 
 		sfRenderWindow_display(window);
 	}
@@ -47,5 +48,5 @@ int main() {
 	sfCircleShape_destroy(circle);
 	sfRenderWindow_destroy(window);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
